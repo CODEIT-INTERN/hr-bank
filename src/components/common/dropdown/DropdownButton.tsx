34cfd6fa87d2e0b09d6a09a81e7baa3f71797b28
Song Jihyun <141377745+songdaramji@users.noreply.github.com>
2025-12-04 01:38:54 +0900

    [FEAT] DropdownButton 컴포넌트 구현 및 문서 추가 (#10)
    * feat: styles css 색상 추가 및 수정
    
    * feat: Button 컴포넌트 구현 및 문서
    
    * feat: 컴포넌트 테스트 페이지 추가
    
    * refactor: Button 컴포넌트 오른쪽 아이콘 props 추가
    
    * feat: 재직 상태 enum 및 label 추가
    
    * feat: DropdownButton 컴포넌트 구현 및 문서 추가
    
    * refactor: TestComponent에 DropdownButton 컴포넌트 반영
    
    * fix: 충돌 해결용 import 수정

---
+++ src/components/common/dropdown/DropdownButton.tsx
@@ -0,0 +1,47 @@
+import { ChevronDown, Check } from "@untitledui/icons";
+import { Dropdown } from "./dropdown";
+import { useState } from "react";
+import { Button } from "../buttons/Button";
+
+interface DropdownButtonProps {
+  placeholder?: string;
+  label: Record<string, string>;
+  onChange?: (value: string) => void;
+}
+
+export const DropdownButton = ({
+  placeholder,
+  label,
+  onChange,
+}: DropdownButtonProps) => {
+  const [selectedKey, setSelectedKey] = useState<string | null>(null);
+
+  const selectedLabel = (selectedKey && label[selectedKey]) || placeholder;
+
+  return (
+    <Dropdown>
+      <Button className="group" color="secondary" iconTrailing={ChevronDown}>
+        {selectedLabel}
+      </Button>
+      <Dropdown.Popover>
+        <Dropdown.Menu>
+          <Dropdown.Section>
+            {Object.entries(label).map(([key, value]) => (
+              <Dropdown.Item
+                key={key}
+                id={key}
+                onClick={() => {
+                  setSelectedKey(key);
+                  onChange?.(key);
+                }}
+                icon={selectedKey === key ? Check : undefined}
+              >
+                {value}
+              </Dropdown.Item>
+            ))}
+          </Dropdown.Section>
+        </Dropdown.Menu>
+      </Dropdown.Popover>
+    </Dropdown>
+  );
+};

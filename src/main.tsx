a8be06e4e75fd7a369d33c8dbc1a45f50a3f12f2
numi8462 <yhk8462@naver.com>
2025-12-03 20:30:51 +0900

    chore(styles): switch Pretendard to local package import

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "pretendard/dist/web/static/pretendard.css";
 import "@/styles/index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Dashboard from "@/pages/Dashboard.tsx";

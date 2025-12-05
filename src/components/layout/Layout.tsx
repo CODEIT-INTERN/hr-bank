5f4a87c16c7e9e685b7134fc0c2d1112685fa8da
songdaramji <songdaramji@gmail.com>
2025-12-05 18:49:10 +0900

    refactor: Layout outlet영역 max witdh, mx auto 제거

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -9,11 +9,9 @@ export default function Layout() {
       <Navigation />
 
       {/* 메인 컨텐츠 영역 */}
-      <main className="flex-1 flex flex-col py-[100px] px-[60px]">
+      <main className="flex-1 flex flex-col py-[100px] px-[60px] gap-7">
         <Header />
-        <div className="max-w-6xl">
-          <Outlet />
-        </div>
+        <Outlet />
       </main>
     </div>
   );

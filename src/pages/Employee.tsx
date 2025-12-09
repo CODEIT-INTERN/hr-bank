0952125c149292ba5d2cbedc0e4a990d3585ea27
songdaramji <songdaramji@gmail.com>
2025-12-10 00:38:45 +0900

    refactor: 직원 관리 페이지 구조 변경

--- src/pages/Employee.tsx
+++ src/pages/Employee.tsx
@@ -1,8 +1,13 @@
+import EmployeeFilterSection from "@/components/employee/EmployeeFilterSection";
 import EmployeeTable from "@/components/employee/EmployeeTable";
-import EmployeeWrapper from "@/components/employee/EmployeeWrapper";
-import TestComponents from "@/components/employee/TestComponents";
 
-// 직원 관리 페이지
-export default function Employee() {
-  return <EmployeeWrapper />;
-}
+const Employee = () => {
+  return (
+    <div className="flex flex-col gap-4 pb-[116px]">
+      <EmployeeFilterSection />
+      <EmployeeTable />
+    </div>
+  );
+};
+
+export default Employee;

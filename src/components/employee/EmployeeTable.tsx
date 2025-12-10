3527d37c3b0e11fb6b1a9c4e1f5c86910ba89b8f
songdaramji <songdaramji@gmail.com>
2025-12-11 03:24:00 +0900

    refactor: 직원관리, 수정이력 테이블 가로 세로 스크롤 적용

--- src/components/employee/EmployeeTable.tsx
+++ src/components/employee/EmployeeTable.tsx
@@ -15,7 +15,15 @@ import CreateUpdateEmployeeModal from "./CreateUpdateEmployeeModal";
 import { AvatarLabelGroup } from "../common/avatar/AvatarLabelGroup";
 
 const EmployeeTable = () => {
-  const { items, isLoading, errorMessage, filters, hasNext, loadFirstPage, loadNextPage } = useEmployeeListStore();
+  const {
+    items,
+    isLoading,
+    errorMessage,
+    filters,
+    hasNext,
+    loadFirstPage,
+    loadNextPage,
+  } = useEmployeeListStore();
 
   // 기본 정렬값(입사일)
   const [sortDescriptor, setSortDescriptor] = useState<SortDescriptor>({
@@ -27,7 +35,9 @@ const EmployeeTable = () => {
   const [targetEmployeeId, setTargetEmployeeId] = useState<number | null>(null);
   // 수정 모달
   const [isUpdateModalOpen, setUpdateModalOpen] = useState(false);
-  const [updatingEmployee, setUpdatingEmployee] = useState<EmployeeDto | null>(null);
+  const [updatingEmployee, setUpdatingEmployee] = useState<EmployeeDto | null>(
+    null
+  );
 
   useEffect(() => {
     loadFirstPage();
@@ -73,91 +83,117 @@ const EmployeeTable = () => {
   };
 
   return (
-    <>
-      <Table aria-label="직원 목록" sortDescriptor={sortDescriptor} onSortChange={setSortDescriptor}>
-        <Table.Header>
-          <Table.Head id="name" label="이름" isRowHeader allowsSorting />
-          <Table.Head id="employeeNumber" label="사원번호" allowsSorting />
-          <Table.Head id="departmentName" label="부서명" />
-          <Table.Head id="position" label="직함" />
-          <Table.Head id="hireDate" label="입사일" allowsSorting />
-          <Table.Head id="status" label="재직상태" />
-          <Table.Head id="actions" />
-        </Table.Header>
-        <Table.Body items={sortedItems}>
-          {(item) => {
-            return (
-              <Table.Row id={item.id} key={item.id}>
-                {/* 이름 + 사번 */}
-                <Table.Cell>
-                  <AvatarLabelGroup
-                    size="md"
-                    src={`/api/files/${item.profileImageId}/download`}
-                    alt={`${item.name}의 프로필`}
-                    title={item.name}
-                    subtitle={item.email}
-                  />
-                </Table.Cell>
-
-                {/* 사원번호 */}
-                <Table.Cell className="whitespace-nowrap">{item.employeeNumber}</Table.Cell>
-
-                {/* 부서명 */}
-                <Table.Cell className="whitespace-nowrap">{item.departmentName}</Table.Cell>
-
-                {/* 직함 */}
-                <Table.Cell className="whitespace-nowrap">{item.position}</Table.Cell>
-
-                {/* 입사일 */}
-                <Table.Cell className="whitespace-nowrap">{formatDateAsKorean(item.hireDate)}</Table.Cell>
-
-                {/* 상태 */}
-                <Table.Cell>
-                  <StatusBadge kind="employment" value={item.status} />
-                </Table.Cell>
-
-                {/* 액션 버튼 */}
-                <Table.Cell className="px-4">
-                  <div className="flex justify-end gap-0.5">
-                    <Button
-                      color="tertiary"
-                      iconLeading={Trash01}
-                      onClick={() => handleOpenConfirmModal(item.id, item.name)}
+    <div className="flex flex-col h-full min-h-0">
+      {/* 테이블 영역 - 가로 스크롤 적용 */}
+      <div className="overflow-auto flex-1 border border-border-secondary rounded-2xl">
+        {" "}
+        <Table
+          aria-label="직원 목록"
+          sortDescriptor={sortDescriptor}
+          onSortChange={setSortDescriptor}
+        >
+          <Table.Header>
+            <Table.Head id="name" label="이름" isRowHeader allowsSorting />
+            <Table.Head id="employeeNumber" label="사원번호" allowsSorting />
+            <Table.Head id="departmentName" label="부서명" />
+            <Table.Head id="position" label="직함" />
+            <Table.Head id="hireDate" label="입사일" allowsSorting />
+            <Table.Head id="status" label="재직상태" />
+            <Table.Head id="actions" />
+          </Table.Header>
+          <Table.Body items={sortedItems}>
+            {(item) => {
+              return (
+                <Table.Row id={item.id} key={item.id}>
+                  {/* 이름 + 사번 */}
+                  <Table.Cell>
+                    <AvatarLabelGroup
+                      size="md"
+                      src={`/api/files/${item.profileImageId}/download`}
+                      alt={`${item.name}의 프로필`}
+                      title={item.name}
+                      subtitle={item.email}
                     />
-                    <Button color="tertiary" iconLeading={Edit01} onClick={() => handleOpenUpdateModal(item)} />
-                  </div>
-                </Table.Cell>
-              </Table.Row>
-            );
-          }}
-        </Table.Body>
-      </Table>
+                  </Table.Cell>
 
-      <div ref={loadMoreRef} />
-      {/* 수정 모달 */}
-      <CreateUpdateEmployeeModal
-        employee={updatingEmployee}
-        isOpen={isUpdateModalOpen}
-        onOpenChange={setUpdateModalOpen}
-      />
-      {/* 삭제 모달 */}
-      <ConfirmModal isOpen={isDeleteModalOpen} onClose={() => setDeleteModalOpen(false)} onConfirm={handleDelete}>
-        <p>
-          ‘{targetEmployeeName}’ 직원을 삭제할까요?
-          <br />
-          삭제 후에는 되돌릴 수 없어요
-        </p>
-      </ConfirmModal>
+                  {/* 사원번호 */}
+                  <Table.Cell className="whitespace-nowrap">
+                    {item.employeeNumber}
+                  </Table.Cell>
 
-      {!isLoading && sortedItems.length === 0 && (
-        <div className="flex justify-center items-center h-48 text-gray-500">현재 표시할 직원이 없습니다</div>
-      )}
-      <div className="flex items-center justify-center text-xs text-gray-500">
-        {/* TODO: 에러처리, 로딩상태 */}
-        {/* <div>{errorMessage && <span className="text-red-500">{errorMessage}</span>}</div> */}
-        {/* {isLoading && <span>불러오는 중...</span>} */}
+                  {/* 부서명 */}
+                  <Table.Cell className="whitespace-nowrap">
+                    {item.departmentName}
+                  </Table.Cell>
+
+                  {/* 직함 */}
+                  <Table.Cell className="whitespace-nowrap">
+                    {item.position}
+                  </Table.Cell>
+
+                  {/* 입사일 */}
+                  <Table.Cell className="whitespace-nowrap">
+                    {formatDateAsKorean(item.hireDate)}
+                  </Table.Cell>
+
+                  {/* 상태 */}
+                  <Table.Cell>
+                    <StatusBadge kind="employment" value={item.status} />
+                  </Table.Cell>
+
+                  {/* 액션 버튼 */}
+                  <Table.Cell className="px-4">
+                    <div className="flex justify-end gap-0.5">
+                      <Button
+                        color="tertiary"
+                        iconLeading={Trash01}
+                        onClick={() =>
+                          handleOpenConfirmModal(item.id, item.name)
+                        }
+                      />
+                      <Button
+                        color="tertiary"
+                        iconLeading={Edit01}
+                        onClick={() => handleOpenUpdateModal(item)}
+                      />
+                    </div>
+                  </Table.Cell>
+                </Table.Row>
+              );
+            }}
+          </Table.Body>
+        </Table>
+        <div ref={loadMoreRef} />
+        {/* 수정 모달 */}
+        <CreateUpdateEmployeeModal
+          employee={updatingEmployee}
+          isOpen={isUpdateModalOpen}
+          onOpenChange={setUpdateModalOpen}
+        />
+        {/* 삭제 모달 */}
+        <ConfirmModal
+          isOpen={isDeleteModalOpen}
+          onClose={() => setDeleteModalOpen(false)}
+          onConfirm={handleDelete}
+        >
+          <p>
+            ‘{targetEmployeeName}’ 직원을 삭제할까요?
+            <br />
+            삭제 후에는 되돌릴 수 없어요
+          </p>
+        </ConfirmModal>
+        {!isLoading && sortedItems.length === 0 && (
+          <div className="flex justify-center items-center h-48 text-gray-500">
+            현재 표시할 직원이 없습니다
+          </div>
+        )}
+        <div className="flex items-center justify-center text-xs text-gray-500">
+          {/* TODO: 에러처리, 로딩상태 */}
+          {/* <div>{errorMessage && <span className="text-red-500">{errorMessage}</span>}</div> */}
+          {/* {isLoading && <span>불러오는 중...</span>} */}
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
--- src/components/history/HistoryTable.tsx
+++ src/components/history/HistoryTable.tsx
@@ -3,10 +3,10 @@ import { Table } from "../common/table/Table";
 import type { SortDescriptor } from "react-aria-components";
 import { sortByDescriptor } from "@/utils/sort";
 import { Button } from "../common/buttons/Button";
-import { Edit01, SearchMd, Trash01 } from "@untitledui/icons";
+import { SearchMd } from "@untitledui/icons";
 import { StatusBadge } from "../common/badges/StatusBadge";
 import { useInfiniteScroll } from "@/hooks/use-infinite-scroll";
-import { formatDateAsKorean, formatDateValue } from "@/utils/date";
+import { formatIsoToYmdHms } from "@/utils/date";
 import type { HistoryDto } from "@/model/history";
 import { useHistoryListStore } from "@/store/historyStore";
 import HIstoryDetailModal from "./HIstoryDetailModal";
@@ -62,89 +62,93 @@ const HistoryTable = () => {
   };
 
   return (
-    <>
-      <Table
-        aria-label="직원 수정 이력 목록"
-        sortDescriptor={sortDescriptor}
-        onSortChange={setSortDescriptor}
-      >
-        <Table.Header>
-          <Table.Head id="type" label="유형" className="w-32" isRowHeader />
-          <Table.Head id="name" label="이름" className="w-24" />
-          <Table.Head id="employeeNumber" label="사원번호" className="w-64" />
-          {/* <Table.Head id="employeeNumber" label="사원번호" /> */}
-          <Table.Head id="memo" label="변경상세내용" />
-          <Table.Head
-            id="ipAddress"
-            label="IP주소"
-            className="w-48"
-            allowsSorting
-          />
-          <Table.Head id="at" label="수정일" className="w-56" allowsSorting />
-          <Table.Head id="actions" className="w-20" />
-        </Table.Header>
-        <Table.Body items={sortedItems}>
-          {(item: HistoryDto) => {
-            return (
-              <Table.Row id={item.id} key={item.id}>
-                {/* 유형 */}
-                <Table.Cell>
-                  <StatusBadge kind="history" value={item.type} />
-                </Table.Cell>
+    <div className="flex flex-col h-full min-h-0">
+      {/* 테이블 영역 - 가로 스크롤 적용 */}
+      <div className="overflow-auto flex-1 border border-border-secondary rounded-2xl">
+        {" "}
+        <Table
+          aria-label="직원 수정 이력 목록"
+          sortDescriptor={sortDescriptor}
+          onSortChange={setSortDescriptor}
+        >
+          <Table.Header>
+            <Table.Head id="type" label="유형" className="w-32" isRowHeader />
+            <Table.Head id="name" label="이름" className="w-24" />
+            <Table.Head id="employeeNumber" label="사원번호" className="w-64" />
+            {/* <Table.Head id="employeeNumber" label="사원번호" /> */}
+            <Table.Head id="memo" label="변경상세내용" />
+            <Table.Head
+              id="ipAddress"
+              label="IP주소"
+              className="w-48"
+              allowsSorting
+            />
+            <Table.Head id="at" label="수정일" className="w-56" allowsSorting />
+            <Table.Head id="actions" className="w-20" />
+          </Table.Header>
+          <Table.Body items={sortedItems}>
+            {(item: HistoryDto) => {
+              return (
+                <Table.Row id={item.id} key={item.id}>
+                  {/* 유형 */}
+                  <Table.Cell>
+                    <StatusBadge kind="history" value={item.type} />
+                  </Table.Cell>
 
-                {/* 이름 */}
-                <Table.Cell className="whitespace-nowrap">이름</Table.Cell>
+                  {/* 이름 */}
+                  <Table.Cell className="whitespace-nowrap">이름</Table.Cell>
 
-                {/* 사원번호 */}
-                <Table.Cell className="whitespace-nowrap">
-                  {item.employeeNumber}
-                </Table.Cell>
+                  {/* 사원번호 */}
+                  <Table.Cell className="whitespace-nowrap">
+                    {item.employeeNumber}
+                  </Table.Cell>
 
-                {/* 변경상세내용 */}
-                <Table.Cell className="whitespace-nowrap flex-grow">
-                  {item.memo}
-                </Table.Cell>
+                  {/* 변경상세내용 */}
+                  <Table.Cell className="whitespace-nowrap w-full">
+                    {item.memo}
+                  </Table.Cell>
 
-                {/* IP주소 */}
-                <Table.Cell className="whitespace-nowrap">
-                  {item.ipAddress}
-                </Table.Cell>
+                  {/* IP주소 */}
+                  <Table.Cell className="whitespace-nowrap">
+                    {item.ipAddress}
+                  </Table.Cell>
 
-                {/* 수정일 */}
-                <Table.Cell className="whitespace-nowrap">
-                  {formatDateAsKorean(item.at)}
-                </Table.Cell>
+                  {/* 수정일 */}
+                  <Table.Cell className="whitespace-nowrap">
+                    {formatIsoToYmdHms(item.at)}
+                  </Table.Cell>
 
-                {/* 액션 버튼 */}
-                <Table.Cell className="px-4 flex justify-center">
-                  <Button
-                    color="tertiary"
-                    iconLeading={SearchMd}
-                    onClick={() => handleOpenDetailModal(item.id)}
-                  />
-                </Table.Cell>
-              </Table.Row>
-            );
-          }}
-        </Table.Body>
-      </Table>
-      <div ref={loadMoreRef} />
-      <HIstoryDetailModal
-        history={selectedHistory}
-        isOpen={isDetailModalOpen}
-        onOpenChange={setDetailModalOpen}
-      />
-      {!isLoading && sortedItems.length === 0 && (
-        <div className="flex justify-center items-center h-48 text-gray-500">
-          현재 표시할 이력이 없습니다
+                  {/* 액션 버튼 */}
+                  <Table.Cell className="px-4 flex justify-center">
+                    <Button
+                      color="tertiary"
+                      iconLeading={SearchMd}
+                      onClick={() => handleOpenDetailModal(item.id)}
+                    />
+                  </Table.Cell>
+                </Table.Row>
+              );
+            }}
+          </Table.Body>
+        </Table>
+        <div ref={loadMoreRef} />
+        <HIstoryDetailModal
+          history={selectedHistory}
+          isOpen={isDetailModalOpen}
+          onOpenChange={setDetailModalOpen}
+        />
+        {!isLoading && sortedItems.length === 0 && (
+          <div className="flex justify-center items-center h-48 text-gray-500">
+            현재 표시할 이력이 없습니다
+          </div>
+        )}
+        <div className="flex items-center justify-center text-xs text-gray-500">
+          {/* TODO: 데이터 없을 때, 에러처리, 로딩상태 */}
+          {/* <div>{errorMessage && <span className="text-red-500">{errorMessage}</span>}</div> */}
+          {/* {isLoading && <span>불러오는 중...</span>} */}
         </div>
-      )}
-      <div className="flex items-center justify-center text-xs text-gray-500">
-        {/* TODO: 데이터 없을 때, 에러처리, 로딩상태 */}
-        {/* <div>{errorMessage && <span className="text-red-500">{errorMessage}</span>}</div> */}
-        {/* {isLoading && <span>불러오는 중...</span>} */}
       </div>
-    </>
+    </div>
   );
 };
 
